1fdca9dd2570ee0f99cde39f29ae1ded3636c6fe
Sameer <msameermh767@gmail.com>
2023-11-18 12:48:40 +0530

    first commit

---
+++ prac1.js
@@ -0,0 +1,60 @@
+// let a = "sameer"
+// let b = 8
+// console.log(a+b)
+
+// // typeof //
+// console.log(typeof(a+b))
+
+// using const //
+// const a= {
+//     name: 'sam',
+//     age: 24,
+// }
+
+// a['friend']= "hehehe"
+// a['status']= "high"
+// a['status']= 'gffgfg'
+// console.log(a)
+
+// dictionary words//
+// const wordsmean = {
+//     carousel: "a merry-go-round at a fair.",
+//     heartfelt: "deeply and strongly felt; sincere",
+//     honesty: "the quality of being honest.",
+//     facet: "one side of something many-sided, especially of a cut gem.",
+//     jewel: "an ornament or piece of jewellery containing a precious stone or stones."
+// }
+
+// console.log(wordsmean['facet'])
+
+// Expressions and Operators//
+
+let a = 74;
+b = 6;
+
+// console.log('a+b=', a+b)
+// console.log('a-b=', a-b)
+// console.log('a/b=', a/b)
+// console.log('a**b=', a**b)
+// console.log('a%b=', a%b)
+// console.log('b++=', b++)
+// console.log('b--=', b--)
+// console.log('b =', b)
+// console.log('a-b=', a-b)
+// console.log('a-b=', a-b)
+// console.log('a-b=', a-b)
+// console.log('a-b=', a-b)
+
+// Assigment Operator//
+// a += 4
+// a -=8
+// a *=8
+// a /=8
+// a %=8
+// a **=8
+// a += b
+// a %= b
+// a **= b
+
+// console.log(a)
+
