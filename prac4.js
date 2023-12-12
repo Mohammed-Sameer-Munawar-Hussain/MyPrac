4e45a30fc1b99b7c123907865d9d6d1b48d69da0
Sameer <msameermh767@gmail.com>
2023-12-12 08:58:08 +0530

    Strings

---
+++ prac4.js
@@ -0,0 +1,27 @@
+let a = "babool" 
+b = "jameel"
+// console.log(a[0])
+// console.log(a[3])
+// console.log(a[4])
+// console.log(a[2])
+// console.log(a.length)
+// console.log(a) // gets in a new line if we use /n'
+// console.log(b) // get some amt of space if we use /t'
+// console.log (`${a} is a frd of ${b}`)
+// console.log(a.toUpperCase())
+// console.log(b.toLowerCase())
+// console.log(b.slice(1))
+// console.log(b.trim())
+// console.log(b.concat(" is a gentelmen but ", a, " is not"))
+// console.log(b.replace("jameel", "khaleel"))
+
+// for (i=0; i <= a.length; i++){
+//     console.log(i)
+// }
+
+// let h = "ladjfa jlfdja loop alfjd adfkjj ldafjhliarg, adfuhufhuuhdfab afdhlhjdavo"
+// c = "loop"
+// console.log(`the word loop ${h.includes(c) ? "is" : "is not"} there in the sentence.`)
+// console.log(a.startsWith("b"))
+// console.log(a.endsWith("L"))
+
