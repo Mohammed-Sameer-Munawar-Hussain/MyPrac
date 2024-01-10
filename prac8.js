47dae332f266b037c6c6aa1089b28e0dcf79e7f6
Sameer <msameermh767@gmail.com>
2024-01-10 11:20:46 +0530

    attributes,insertion and class methods

---
+++ prac8.js
@@ -0,0 +1,33 @@
+// Attributes Methods //
+// let a = document.getElementById("asdf")
+// // console.log(a)
+// let b = asdf.getAttribute("class")// to get the element through calling the class
+// // console.log(b)
+// // console.log(a.hasAttribute("class")) // checks for the class/id(attributes) existance in a given element
+// // console.log(a.hasAttribute("style"))
+// // a.setAttribute("id", "d23")// to change or replace the value of an attribute
+// a.removeAttribute("id")// to remove attribute from an element
+// // console.log(a.attributes)// will show all the attributes
+// // console.log(a.dataset)
+// // console.log(a.dataset.heymant)
+// console.log(a.dataset.batman) can create own attributes by using data-=""
+
+// Insertion methods //
+// let a = document.getElementsByTagName("div")[0]
+// // console.log(a)
+// // a.innerHTML = a.innerHTML + " <div>additional div</div>";
+// let b = document.createElement("div")
+// b.innerHTML = "<h2>heading 2</h2>"
+// // a.appendChild(b)// it will add a new div(new child) to the parent div
+// // a.append(b)//
+// // a.prepend(b)//
+// // a.before(b)//
+// // a.after(b)//
+// a.replaceWith(b)//
+
+// className and classlist//
+// rd.className= "texts red"
+// // rd.classList.remove ('texts')
+// // rd.classList.add ('texts')
+// // rd.classList.toggle ('texts')
+// rd.classList.contains ('yellow')
